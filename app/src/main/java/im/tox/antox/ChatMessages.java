2f9efdbc7e01961af7cea5e3aafa9df25fdf686d
Aagam Shah <aagam94@gmail.com>
2014-02-27 02:13:38 +0530

    modified ChatActivity

--- app/src/main/java/im/tox/antox/ChatMessages.java
+++ app/src/main/java/im/tox/antox/ChatMessages.java
@@ -1,27 +1,30 @@
 package im.tox.antox;
 
 public class ChatMessages {
-	public String message;
-	public boolean ownMessage;
-	public String friendName;
+    public String message;
+    public String time;
+    public boolean ownMessage;
+    public String friendName;
 
-	public ChatMessages() {
-		super();
-	}
+    public ChatMessages() {
+        super();
+    }
 
-	public ChatMessages(String message, boolean ownMessage) {
-		super();
-		this.message = message;
-		this.ownMessage = ownMessage;
-	}
+    public ChatMessages(String message, String time, boolean ownMessage) {
+        super();
+        this.time = time;
+        this.message = message;
+        this.ownMessage = ownMessage;
+    }
 
-	public ChatMessages(String message, String friendName) {
-		super();
-		this.message = message;
-		this.friendName = friendName;
-	}
+    public ChatMessages(String message, String time, String friendName) {
+        super();
+        this.time = time;
+        this.message = message;
+        this.friendName = friendName;
+    }
 
-	public boolean IsMine() {
-		return ownMessage;
-	}
+    public boolean IsMine() {
+        return ownMessage;
+    }
 }
