31788af2417b749a46d44aa7e9f54e91544ce9d5
dethstar <pcepedam92@gmail.com>
2014-03-11 23:14:07 -0600

    add braces

--- app/src/main/java/im/tox/antox/AntoxFriendList.java
+++ app/src/main/java/im/tox/antox/AntoxFriendList.java
@@ -59,14 +59,15 @@ public class AntoxFriendList implements FriendList<AntoxFriend> {
 
 	@Override
 	public AntoxFriend getById(String id) {
-        if(id!=null)
-		synchronized (this.friends) {
-			for (AntoxFriend friend : this.friends) {
-				if (id.equals(friend.getId())) {
-					return friend;
-				}
-			}
-		}
+        if(id!=null) {
+            synchronized (this.friends) {
+                for (AntoxFriend friend : this.friends) {
+                    if (id.equals(friend.getId())) {
+                        return friend;
+                    }
+                }
+            }
+        }
 		return null;
 	}
 
