ecd94d924f2f8590d94e8cab6cd15872aff68dce
Dragos Corlatescu <dragos.corlatescu@gmail.com>
2014-04-12 21:05:20 +0300

    Modify Friend Request Fragment so in case of an accept the friend can be placed in a group.

--- app/src/main/java/im/tox/antox/fragments/FriendRequestFragment.java
+++ app/src/main/java/im/tox/antox/fragments/FriendRequestFragment.java
@@ -1,13 +1,20 @@
 package im.tox.antox.fragments;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 import im.tox.antox.data.AntoxDB;
 import im.tox.antox.utils.Constants;
@@ -47,6 +54,22 @@ public class FriendRequestFragment extends Fragment {
         k.setText(SplitKey(key));
         TextView m = (TextView) rootView.findViewById(R.id.requestfragment_message);
         m.setText(message);
+        //set the spinner
+        final Spinner friendGroup = (Spinner) rootView.findViewById(R.id.spinner_accept_friend_request_group);
+        ArrayList<String> spinnerArray = new ArrayList<String>();
+        spinnerArray.add(getResources().getString(R.string.manage_groups_friends));
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("groups", Context.MODE_PRIVATE);
+        if (!sharedPreferences.getAll().isEmpty()) {
+            Map<String,?> keys = sharedPreferences.getAll();
+
+            for(Map.Entry<String,?> entry : keys.entrySet()){
+                String groupName = entry.getValue().toString();
+                spinnerArray.add(groupName);
+            }
+        }
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, spinnerArray);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        friendGroup.setAdapter(spinnerAdapter);
         Button accept = (Button) rootView.findViewById(R.id.acceptFriendRequest);
         Button reject = (Button) rootView.findViewById(R.id.rejectFriendRequest);
         accept.setOnClickListener(new View.OnClickListener() {
@@ -54,7 +77,8 @@ public class FriendRequestFragment extends Fragment {
             public void onClick(View view) {
                 getActivity().getSupportFragmentManager().popBackStack();
                 AntoxDB db = new AntoxDB(getActivity().getApplicationContext());
-                db.addFriend(key, "Friend Accepted", "", "");
+                String group = friendGroup.getSelectedItem().toString();
+                db.addFriend(key, "Friend Accepted", "", "", group);
                 db.close();
                 ((MainActivity) getActivity()).updateLeftPane();
                 ((MainActivity) getActivity()).pane.openPane();
