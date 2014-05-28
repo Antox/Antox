e4b53b74ea60028c3b617dda03bf2acd383b1a3a
astonex <softukitu@gmail.com>
2014-05-28 05:24:03 +0100

    Combine settings and profile activities into a Preference Activity

--- app/src/main/java/im/tox/antox/utils/Constants.java
+++ app/src/main/java/im/tox/antox/utils/Constants.java
@@ -6,20 +6,14 @@ public final class Constants {
     public static final int TYPE_FRIEND_REQUEST = 1;
     public static final int TYPE_CONTACT = 2;
     public static final int TYPE_MAX_COUNT = 3;
-	public static final String REGISTER = "im.tox.antox.REGISTER";
-	public static final String UNREGISTER = "im.tox.antox.UNREGISTER";
-	public static final String REGISTER_NAME = "im.tox.antox.REGISTER_NAME";
     public static final String START_TOX = "im.tox.antox.START_TOX";
     public static final String STOP_TOX = "im.tox.antox.STOP_TOX";
     public static final String BROADCAST_ACTION = "im.tox.antox.BROADCAST";
-    public static final String CONNECTED_STATUS = "im.tox.antox.STATUS";
     public static final String ADD_FRIEND = "im.tox.antox.ADD";
-    public static final String UPDATE_SETTINGS = "im.tox.antox.SETTINGS";
     public static final String FRIEND_LIST = "im.tox.antox.FRIENDS";
     public static final String SWITCH_TO_FRIEND = "im.tox.antox.SWITCH_TO_FRIEND";
     public static final String SEND_MESSAGE ="im.tox.antox.MESSAGE";
     public static final String UPDATE_MESSAGES ="im.tox.antox.UPDATE_MESSAGES";
-    public static final String CLEAR_NOTIFICATIONS_FOR_FRIENDNUMBER  = "im.tox.antox.CLEAR_NOTIFICATIONS_FOR_FRIENDNUMBER";
     public static final String FRIEND_REQUEST = "im.tox.antox.REQUEST";
     public static final String DELETE_FRIEND = "im.tox.antox.DELETE_FRIEND";
     public static final String DELIVERY_RECEIPT = "im.tox.antox.DELIVERY_RECEIPT";
@@ -28,7 +22,6 @@ public final class Constants {
     public static final String REJECT_FRIEND_REQUEST = "im.tox.antox.REJECT_REQUEST";
     public static final String ACCEPT_FRIEND_REQUEST = "im.tox.antox.ACCEPT_REQUEST";
     public static final String UPDATE_LEFT_PANE = "im.tox.antox.UPDATE_REQUESTS";
-    public static final String CONNECTION_STATUS = "im.tox.antox.CONNECTION_STATUS";
     public static final String SEND_UNSENT_MESSAGES = "im.tox.antox.SEND_UNSENT_MESSAGES";
     public static final String UPDATE = "im.tox.antox.UPDATE";
 
