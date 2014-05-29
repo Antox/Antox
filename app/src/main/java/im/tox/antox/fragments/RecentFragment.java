f4bf4a1eea690f49f3d5b420df7dab89799c3f1e
Oliver Hunt <oliver.huntuk@gmail.com>
2014-05-29 21:34:47 +0100

    Started work on recent convo fragment

--- app/src/main/java/im/tox/antox/fragments/RecentFragment.java
+++ app/src/main/java/im/tox/antox/fragments/RecentFragment.java
@@ -10,10 +10,13 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -24,12 +27,17 @@ import im.tox.antox.R;
 import im.tox.antox.activities.FriendProfileActivity;
 import im.tox.antox.activities.MainActivity;
 import im.tox.antox.adapters.LeftPaneAdapter;
+import im.tox.antox.adapters.RecentAdapter;
 import im.tox.antox.data.AntoxDB;
 import im.tox.antox.tox.ToxService;
 import im.tox.antox.tox.ToxSingleton;
 import im.tox.antox.utils.Constants;
 import im.tox.antox.utils.Friend;
+import im.tox.antox.utils.FriendInfo;
 import im.tox.antox.utils.LeftPaneItem;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
 
 /**
  * Created by ollie on 28/02/14.
@@ -37,6 +45,10 @@ import im.tox.antox.utils.LeftPaneItem;
 public class RecentFragment extends Fragment {
 
     ToxSingleton toxSingleton = ToxSingleton.getInstance();
+
+    private ListView conversationListView;
+    private ArrayAdapter<FriendInfo> conversationAdapter;
+    private Subscription sub;
 
     public RecentFragment() {
     }
@@ -46,7 +58,40 @@ public class RecentFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_recent, container, false);
+        conversationListView = (ListView) rootView.findViewById(R.id.conversations_list);
         return rootView;
     }
 
+    @Override
+    public void onResume(){
+        super.onResume();
+        sub = toxSingleton.friendInfoListSubject.observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<ArrayList<FriendInfo>>() {
+                    @Override
+                    public void call(ArrayList<FriendInfo> friends_list) {
+                        updateRecentConversations(friends_list);
+                    }
+                });
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        sub.unsubscribe();
+    }
+
+    public void updateRecentConversations(ArrayList<FriendInfo> friendsList) {
+
+        //If you have no recent conversations, display  message
+        LinearLayout noConversations = (LinearLayout) getView().findViewById(R.id.recent_no_conversations);
+        if (friendsList.size() == 0) {
+            noConversations.setVisibility(View.VISIBLE);
+        } else {
+            noConversations.setVisibility(View.GONE);
+        }
+
+        conversationAdapter = new RecentAdapter(getActivity(), R.layout.contact_list_item, friendsList);
+        conversationListView.setAdapter(conversationAdapter);
+        System.out.println("updated recent fragment");
+    }
 }
