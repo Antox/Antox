859ec0e58538340328a6d1d846ea042633622faf
astonex <softukitu@gmail.com>
2014-06-04 12:54:38 +0100

    Move callback related functions to the callback classes, removing them from ToxService

--- app/src/main/java/im/tox/antox/activities/AddFriendActivity.java
+++ app/src/main/java/im/tox/antox/activities/AddFriendActivity.java
@@ -19,6 +19,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import org.xbill.DNS.Lookup;
@@ -26,14 +27,18 @@ import org.xbill.DNS.Record;
 import org.xbill.DNS.TXTRecord;
 import org.xbill.DNS.Type;
 
+
 import im.tox.QR.IntentIntegrator;
 import im.tox.QR.IntentResult;
-import im.tox.antox.R;
 import im.tox.antox.data.AntoxDB;
+
 import im.tox.antox.fragments.PinDialogFragment;
-import im.tox.antox.tox.ToxService;
+import im.tox.antox.tox.ToxSingleton;
 import im.tox.antox.utils.Constants;
+import im.tox.antox.R;
 import im.tox.antox.utils.DhtNode;
+import im.tox.jtoxcore.FriendExistsException;
+import im.tox.jtoxcore.ToxException;
 
 public class AddFriendActivity extends ActionBarActivity implements PinDialogFragment.PinDialogListener {
 
@@ -51,6 +56,7 @@ public class AddFriendActivity extends ActionBarActivity implements PinDialogFra
     EditText friendID;
     EditText friendMessage;
     EditText friendAlias;
+    Spinner  friendGroup;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -150,15 +156,20 @@ public class AddFriendActivity extends ActionBarActivity implements PinDialogFra
             String[] friendData = {ID, message, alias};
 
             AntoxDB db = new AntoxDB(getApplicationContext());
-            if (!db.doesFriendExist(friendID.getText().toString())) {
-                Intent addFriend = new Intent(this, ToxService.class);
-                addFriend.setAction(Constants.ADD_FRIEND);
-                addFriend.putExtra("friendData", friendData);
-                this.startService(addFriend);
+            if (!db.doesFriendExist(ID)) {
+                try {
+                    ToxSingleton toxSingleton = ToxSingleton.getInstance();
+                    toxSingleton.jTox.addFriend(friendData[0], friendData[1]);
+                } catch (ToxException e) {
+                    e.printStackTrace();
+                } catch (FriendExistsException e) {
+                    e.printStackTrace();
+                }
 
                 if (!alias.equals(""))
                     ID = alias;
 
+                String group = friendGroup.getSelectedItem().toString();
                 Log.d("AddFriendActivity","Adding friend to database");
                 db.addFriend(ID, "Friend Request Sent", alias, originalUsername);
             } else {
