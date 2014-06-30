26f01c36552d8a85c1bc6a30a90d7be842fdf1e3
Oliver Hunt <oliver.huntuk@gmail.com>
2014-06-30 23:00:35 +0100

    Workaround for Time.set(long) not working as advertised

--- app/src/main/java/im/tox/antox/utils/PrettyTimestamp.java
+++ app/src/main/java/im/tox/antox/utils/PrettyTimestamp.java
@@ -3,7 +3,10 @@ package im.tox.antox.utils;
 import java.sql.Timestamp;
 import java.text.DateFormat;
 import android.text.format.Time;
+import android.util.Log;
+
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 
@@ -11,59 +14,46 @@ import java.util.TimeZone;
  * Created by ollie on 29/05/14.
  */
 public class PrettyTimestamp {
-    public static String prettyTimestamp(Timestamp t) {
-        Time time = new Time();
-        time.set(t.getTime());
-        Time startOfDay = new Time();
+    public static String prettyTimestamp(Timestamp t, boolean isChat) {
+        //Set time in UTC time
+        Time time = new Time("UTC");
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(t);
+        time.set(cal.get(Calendar.SECOND), cal.get(Calendar.MINUTE), cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.DAY_OF_MONTH), cal.get(Calendar.MONTH), cal.get(Calendar.YEAR));
+        //Switch to device timezone
+        time.switchTimezone(Time.getCurrentTimezone());
+        Time startOfDay = new Time(Time.getCurrentTimezone());
         startOfDay.setToNow();
         startOfDay.hour = 0;
         startOfDay.minute = 0;
         startOfDay.second = 0;
-        Time startOfYear = new Time();
+        Time startOfYear = new Time(Time.getCurrentTimezone());
         startOfYear.setToNow();
         startOfYear.hour = 0;
         startOfYear.minute = 0;
         startOfYear.second = 0;
         startOfYear.monthDay = 0;
         startOfYear.month = 0;
-        Time startOfTime = new Time();
+        Time startOfTime = new Time(Time.getCurrentTimezone());
         startOfTime.set(0);
-        if (time.equals(startOfTime)) {
+        if (t.equals(new Timestamp(0,0,0,0,0,0,0))) {
             return "";
-        } else if (time.after(startOfDay)) {
-            return time.format("%H:%M");
-        } else if (time.after(startOfYear)) {
-            return time.format("%b %d");
+        } else if (isChat) {
+            if (time.after(startOfDay)) {
+                return time.format("%H:%M");
+            } else if (time.after(startOfYear)) {
+                return time.format("%b %d, %H:%M");
+            } else {
+                return time.format("%b %d %Y, %H:%M");
+            }
         } else {
-            return time.format("%b %d %Y");
-        }
-    }
-
-    public static String prettyChatTimestamp(Timestamp t) {
-        Time time = new Time();
-        time.set(t.getTime());
-        Time startOfDay = new Time();
-        startOfDay.setToNow();
-        startOfDay.hour = 0;
-        startOfDay.minute = 0;
-        startOfDay.second = 0;
-        Time startOfYear = new Time();
-        startOfYear.setToNow();
-        startOfYear.hour = 0;
-        startOfYear.minute = 0;
-        startOfYear.second = 0;
-        startOfYear.monthDay = 0;
-        startOfYear.month = 0;
-        Time startOfTime = new Time();
-        startOfTime.set(0);
-        if (time.equals(startOfTime)) {
-            return "";
-        } else if (time.after(startOfDay)) {
-            return time.format("%H:%M");
-        } else if (time.after(startOfYear)) {
-            return time.format("%b %d, %H:%M");
-        } else {
-            return time.format("%b %d %Y, %H:%M");
+            if (time.after(startOfDay)) {
+                return time.format("%H:%M");
+            } else if (time.after(startOfYear)) {
+                return time.format("%b %d");
+            } else {
+                return time.format("%b %d %Y");
+            }
         }
     }
 }
