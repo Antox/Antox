856d00f0c4a93b90001ea600362997358b66de96
astonex <softukitu@gmail.com>
2014-07-24 19:19:54 +0100

    Make sure toxSingleton is inited before calling doTox()

--- app/src/main/java/im/tox/antox/tox/ToxDoService.java
+++ app/src/main/java/im/tox/antox/tox/ToxDoService.java
@@ -25,24 +25,17 @@ public class ToxDoService extends Service {
 
     @Override
     public void onCreate() {
-        if(!toxSingleton.isInited) {
-            Runnable initTox = new Runnable() {
-                @Override
-                public void run() {
-                    toxSingleton.initTox(getApplicationContext());
-                }
-            };
-            new Thread(initTox).start();
-        }
-
-        Runnable runnable = new Runnable() {
+        Runnable start = new Runnable() {
             @Override
             public void run() {
+                if(!toxSingleton.isInited)
+                    toxSingleton.initTox(getApplicationContext());
+
                 final DoTox doTox = new DoTox();
                 toxScheduleTaskExecutor.scheduleAtFixedRate(doTox, 0, 50, TimeUnit.MILLISECONDS);
             }
         };
-        new Thread(runnable).start();
+        new Thread(start).start();
     }
 
     @Override
