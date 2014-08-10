c8d98a5933de97dedc7e2ed55e726ca4da39f8af
Oliver Hunt <oliver.huntuk@gmail.com>
2014-08-10 10:27:59 +0100

    Improved recent pane performance, and fixed the sql

--- app/src/main/java/im/tox/antox/fragments/RecentFragment.java
+++ app/src/main/java/im/tox/antox/fragments/RecentFragment.java
@@ -25,6 +25,7 @@ import im.tox.antox.tox.ToxSingleton;
 import im.tox.antox.utils.FriendInfo;
 import rx.Observable;
 import rx.Observer;
+import rx.Subscriber;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
@@ -41,10 +42,11 @@ public class RecentFragment extends Fragment {
     private ListView conversationListView;
     private ArrayAdapter<FriendInfo> conversationAdapter;
     private Subscription sub;
-    private String activeKey;
-    private Subscription keySub;
     private AntoxDB antoxDB;
     private RecentAdapter adapter;
+    private View rootView;
+    private boolean paused;
+    LinearLayout noConversations;
 
     public RecentFragment() {
     }
@@ -52,73 +54,72 @@ public class RecentFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        View rootView = inflater.inflate(R.layout.fragment_recent, container, false);
-        this.antoxDB = new AntoxDB(getActivity());
-        Cursor cursor = this.antoxDB.getRecentCursor();
-        adapter = new RecentAdapter(getActivity(), cursor);
-        conversationListView = (ListView) rootView.findViewById(R.id.conversations_list);
-        conversationListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-        conversationListView.setAdapter(adapter);
+        if (rootView == null) {
+            rootView = inflater.inflate(R.layout.fragment_recent, container, false);
+            conversationListView = (ListView) rootView.findViewById(R.id.conversations_list);
+            conversationListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+            noConversations = (LinearLayout) rootView.findViewById(R.id.recent_no_conversations);
+        } else {
+            ((ViewGroup) rootView.getParent()).removeView(rootView);
+        }
+        Observable.create(new Observable.OnSubscribe<Cursor>() {
+            @Override
+            public void call(Subscriber<? super Cursor> observer) {
+                try {
+                    Cursor cursor = getCursor();
+                    observer.onNext(cursor);
+                    observer.onCompleted();
+                } catch (Exception e) {
+                    observer.onError(e);
+                }
+            }
+        }).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Cursor>() {
+            @Override
+            public void call(Cursor cursor) {
+                if (adapter == null) {
+                    adapter = new RecentAdapter(getActivity(), cursor);
+                    conversationListView.setAdapter(adapter);
+                } else {
+                    adapter.changeCursor(cursor);
+                }
+            }
+        });
         return rootView;
     }
 
     @Override
     public void onResume(){
         super.onResume();
-        sub = toxSingleton.friendInfoListSubject.observeOn(AndroidSchedulers.mainThread())
+        sub = toxSingleton.friendInfoListSubject.observeOn(AndroidSchedulers.mainThread()).distinctUntilChanged()
                 .subscribe(new Action1<ArrayList<FriendInfo>>() {
                     @Override
                     public void call(ArrayList<FriendInfo> friends_list) {
-                        updateRecentConversations(filterSortRecent(friends_list));
-                        //setSelectionToKey(activeKey);
+                        if (!paused) {
+                            updateRecentConversations(friends_list);
+                        }
+                        paused = false;
                     }
                 });
-        keySub = toxSingleton.activeKeySubject.observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Action1<String>() {
-                    @Override
-                    public void call(String s) {
-                        Log.d("RecentFragment", "key subject");
-                        activeKey = s;
-                        //setSelectionToKey(activeKey);
-                    }
-                });
-    }
-
-    private ArrayList<FriendInfo> filterSortRecent(ArrayList<FriendInfo> input) {
-        ArrayList<FriendInfo> temp = new ArrayList<FriendInfo>();
-        for (FriendInfo f: input) {
-            if (!f.lastMessageTimestamp.equals(new Timestamp(0,0,0,0,0,0,0))) {
-                temp.add(f);
-            }
-        }
-        class CustomComparator implements Comparator<FriendInfo> {
-            @Override
-            public int compare(FriendInfo o1, FriendInfo o2) {
-                return o2.lastMessageTimestamp.compareTo(o1.lastMessageTimestamp);
-            }
-        }
-
-        Collections.sort(temp, new CustomComparator());
-
-        return temp;
     }
 
     @Override
     public void onPause(){
         super.onPause();
+        paused = true;
         sub.unsubscribe();
-        keySub.unsubscribe();
     }
 
     private Cursor getCursor() {
+        if (this.antoxDB == null) {
+            this.antoxDB = new AntoxDB(getActivity());
+        }
         Cursor cursor = this.antoxDB.getRecentCursor();
         return cursor;
     }
     public void updateRecentConversations(ArrayList<FriendInfo> friendsList) {
 
         //If you have no recent conversations, display  message
-        LinearLayout noConversations = (LinearLayout) getView().findViewById(R.id.recent_no_conversations);
         if (friendsList.size() == 0) {
             noConversations.setVisibility(View.VISIBLE);
         } else {
