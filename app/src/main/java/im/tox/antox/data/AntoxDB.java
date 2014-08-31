d9a68ec425854b625883ff77258c76a234323dbe
fireflystorm <razni@krutt.org>
2014-08-31 12:26:30 +0100

    Merge remote-tracking branch 'Astonex/master'

--- app/src/main/java/im/tox/antox/data/AntoxDB.java
+++ app/src/main/java/im/tox/antox/data/AntoxDB.java
@@ -2,10 +2,12 @@ package im.tox.antox.data;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import java.sql.Timestamp;
