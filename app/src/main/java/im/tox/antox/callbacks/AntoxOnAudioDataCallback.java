82603a8dcc479a84f384d34b79d94c45199a4df3
astonex <softukitu@gmail.com>
2014-09-05 07:06:06 +0100

    A little more av testing

--- app/src/main/java/im/tox/antox/callbacks/AntoxOnAudioDataCallback.java
+++ app/src/main/java/im/tox/antox/callbacks/AntoxOnAudioDataCallback.java
@@ -1,7 +1,15 @@
 package im.tox.antox.callbacks;
 
 import android.content.Context;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+import android.media.MediaPlayer;
 import android.util.Log;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 import im.tox.antox.utils.AntoxFriend;
 import im.tox.jtoxcore.callbacks.OnAudioDataCallback;
@@ -19,5 +27,21 @@ public class AntoxOnAudioDataCallback implements OnAudioDataCallback<AntoxFriend
 
     public void execute(int callID, byte[] data) {
         Log.d("OnAudioDataCallback", "Received callback from: " + callID);
+        try {
+            AudioTrack audioTrack = new AudioTrack(
+                    AudioManager.STREAM_MUSIC,
+                    48000,
+                    AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT,
+                    data.length,
+                    AudioTrack.MODE_STATIC);
+
+            audioTrack.play();
+            audioTrack.write(data, 0, data.length);
+            audioTrack.stop();
+            audioTrack.flush();
+
+        } catch (Exception e) {
+        }
     }
 }
--- app/src/main/java/im/tox/antox/callbacks/AntoxOnAvCallbackCallback.java
+++ app/src/main/java/im/tox/antox/callbacks/AntoxOnAvCallbackCallback.java
@@ -1,10 +1,15 @@
 package im.tox.antox.callbacks;
 
 import android.content.Context;
+import android.media.AudioManager;
 import android.util.Log;
 
+import im.tox.antox.R;
+import im.tox.antox.data.AntoxDB;
 import im.tox.antox.tox.ToxSingleton;
 import im.tox.antox.utils.AntoxFriend;
+import im.tox.antox.utils.AntoxFriendList;
+import im.tox.jtoxcore.FriendList;
 import im.tox.jtoxcore.ToxAvCallbackID;
 import im.tox.jtoxcore.ToxCallType;
 import im.tox.jtoxcore.ToxCodecSettings;
@@ -25,9 +30,65 @@ public class AntoxOnAvCallbackCallback implements OnAvCallbackCallback<AntoxFrie
     public void execute(int callID, ToxAvCallbackID callbackID) {
         Log.d("OnAvCallbackCallback", "Received a callback from: " + callID);
         ToxSingleton toxSingleton = ToxSingleton.getInstance();
-        ToxCodecSettings toxCodecSettings = new ToxCodecSettings(ToxCallType.TYPE_AUDIO, 0, 0, 0, 64000, 20, 48000, 1);
+
         try {
-            toxSingleton.jTox.avAnswer(0, toxCodecSettings);
+            switch (callbackID) {
+            /* Requests */
+                case ON_INVITE: // Incoming call request
+                    Log.d("OnAvCallbackCallback", "Callback type: ON_INVITE");
+                    // Display UI elements to accept or reject the call
+                    // For testing auto-accept
+                    ToxCodecSettings toxCodecSettings = new ToxCodecSettings(ToxCallType.TYPE_AUDIO, 500, 1280, 720, 64000, 20, 48000, 1);
+                    toxSingleton.jTox.avAnswer(callID, toxCodecSettings);
+                    break;
+
+                case ON_START: // Incoming call was accepted
+                    Log.d("OnAvCallbackCallback", "Callback type: ON_START");
+                    // Prepare for transmission
+                    toxSingleton.jTox.avPrepareTransmission(0, 3, 40, false);
+
+                    break;
+
+                case ON_CANCEL: // Incoming call timed out/stopped
+                    Log.d("OnAvCallbackCallback", "Callback type: ON_CANCEL");
+                    break;
+
+                case ON_REJECT: // Incoming call was rejected
+                    Log.d("OnAvCallbackCallback", "Callback type: ON_REJECT");
+                    break;
+
+                case ON_END: // On-going call has now been ended
+                    Log.d("OnAvCallbackCallback", "Callback type: ON_END");
+                    break;
+
+
+            /* Responses */
+                case ON_RINGING: // Our call has gone through and is ringing
+                    Log.d("OnAvCallbackCallback", "Callback type: ON_RINGING");
+                    break;
+
+                case ON_STARTING:
+                    Log.d("OnAvCallbackCallback", "Callback type: ON_STARTING");
+                    break;
+
+                case ON_ENDING:
+                    Log.d("OnAvCallbackCallback", "Callback type: ON_ENDING");
+                    break;
+
+
+            /* Protocol */
+                case ON_REQUEST_TIMEOUT:
+                    Log.d("OnAvCallbackCallback", "Callback type: ON_REQUEST_TIMEOUT");
+                    break;
+
+                case ON_PEER_TIMEOUT:
+                    Log.d("OnAvCallbackCallback", "Callback type: ON_PEER_TIMEOUT");
+                    break;
+
+                case ON_MEDIA_CHANGE:
+                    Log.d("OnAvCallbackCallback", "Callback type: ON_MEDIA_CHANGE");
+                    break;
+            }
         } catch (ToxException e) {
         }
     }
