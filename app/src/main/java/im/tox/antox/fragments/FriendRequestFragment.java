138b7ac6d158cd6d3c80c86e853e5cd8ed2a452f
Proplex <horo@ipwnage.com>
2014-09-26 00:57:56 -0400

    Remove unused imports

--- app/src/main/java/im/tox/antox/fragments/FriendRequestFragment.java
+++ app/src/main/java/im/tox/antox/fragments/FriendRequestFragment.java
@@ -1,14 +1,11 @@
 package im.tox.antox.fragments;
 
-import android.media.Image;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
