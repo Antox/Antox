2d52de5b26085e7e32b0500331dbbd61673d6e7d
Oliver Hunt <oliver.huntuk@gmail.com>
2014-10-04 12:12:23 +0100

    Fixed my shitty script

--- download-dependencies.sh
+++ download-dependencies.sh
@@ -1,29 +1,8 @@
 #!/bin/bash
-cd ${0%/*}
 mkdir -p app/src/main/jniLibs/armeabi
-cd app/src/main/jniLibs/armeabi
-wget https://jenkins.libtoxcore.so/job/jToxcore_Android/lastSuccessfulBuild/artifact/artifacts/armeabi/libjtoxcore.so
-wget https://jenkins.libtoxcore.so/job/jToxcore_Android/lastSuccessfulBuild/artifact/artifacts/armeabi/libsodium.so
-wget https://jenkins.libtoxcore.so/job/jToxcore_Android/lastSuccessfulBuild/artifact/artifacts/armeabi/libtoxcore.so
-cd ${0%/*}
-mkdir -p app/src/main/jniLibs/armeabi-v7a
-cd app/src/main/jniLibs/armeabi-v7a
-wget https://jenkins.libtoxcore.so/job/jToxcore_Android/lastSuccessfulBuild/artifact/artifacts/armeabi-v7a/libjtoxcore.so
-wget https://jenkins.libtoxcore.so/job/jToxcore_Android/lastSuccessfulBuild/artifact/artifacts/armeabi-v7a/libsodium.so
-wget https://jenkins.libtoxcore.so/job/jToxcore_Android/lastSuccessfulBuild/artifact/artifacts/armeabi-v7a/libtoxcore.so
-cd ${0%/*}
-mkdir -p app/src/main/jniLibs/mips
-cd app/src/main/jniLibs/mips
-wget https://jenkins.libtoxcore.so/job/jToxcore_Android/lastSuccessfulBuild/artifact/artifacts/mips/libjtoxcore.so
-wget https://jenkins.libtoxcore.so/job/jToxcore_Android/lastSuccessfulBuild/artifact/artifacts/mips/libsodium.so
-wget https://jenkins.libtoxcore.so/job/jToxcore_Android/lastSuccessfulBuild/artifact/artifacts/mips/libtoxcore.so
-cd ${0%/*}
-mkdir -p app/src/main/jniLibs/x86
-cd app/src/main/jniLibs/x86
-wget https://jenkins.libtoxcore.so/job/jToxcore_Android/lastSuccessfulBuild/artifact/artifacts/x86/libjtoxcore.so
-wget https://jenkins.libtoxcore.so/job/jToxcore_Android/lastSuccessfulBuild/artifact/artifacts/x86/libsodium.so
-wget https://jenkins.libtoxcore.so/job/jToxcore_Android/lastSuccessfulBuild/artifact/artifacts/x86/libtoxcore.so
+rm app/src/main/jniLibs/armeabi/libtoxcore.so
+wget https://jenkins.libtoxcore.so/job/jtoxcore_android_arm/lastSuccessfulBuild/artifact/artifacts/armeabi/libjtoxcore.so -O app/src/main/jniLibs/armeabi/libjtoxcore.so
 cd ${0%/*}
 mkdir -p app/libs
-cd app/libs
-wget https://jenkins.libtoxcore.so/job/jToxcore_Android/lastSuccessfulBuild/artifact/artifacts/jToxcore.jar
+rm app/libs/jToxcore.jar
+wget https://jenkins.libtoxcore.so/job/jtoxcore_android_arm/lastSuccessfulBuild/artifact/artifacts/jToxcore.jar -O app/libs/jToxcore.jar
