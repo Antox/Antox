2976b0b4bfb29594e0f986d1ffacf71ac83f063f
subliun <subliundev@gmail.com>
2015-03-12 16:21:04 +1030

    Fix Antox on 2.3.x

--- app/src/main/scala/im/tox/antox/fragments/LeftPaneFragment.scala
+++ app/src/main/scala/im/tox/antox/fragments/LeftPaneFragment.scala
@@ -1,11 +1,11 @@
 package im.tox.antox.fragments
 
 import android.app.{ActionBar, FragmentTransaction}
-import android.os.Bundle
+import android.os.{Build, Bundle}
 import android.support.v4.app.{Fragment, FragmentManager, FragmentPagerAdapter}
 import android.support.v4.view.ViewPager
 import android.view.{LayoutInflater, View, ViewGroup}
-import android.widget.ImageView
+import android.widget.{FrameLayout, ImageView}
 import com.astuetz.PagerSlidingTabStrip
 import com.astuetz.PagerSlidingTabStrip.CustomTabProvider
 import com.balysv.materialripple.MaterialRippleLayout
@@ -19,10 +19,17 @@ class LeftPaneFragment extends Fragment {
     val ICONS: Array[Int] = Array(R.drawable.ic_action_recent_tab, R.drawable.ic_action_contacts_tab)
 
     override def getCustomTabView(parent: ViewGroup, position: Int): View = {
-         val materialRippleLayout: MaterialRippleLayout = LayoutInflater.from(getActivity)
-            .inflate(R.layout.custom_tab, parent, false).asInstanceOf[MaterialRippleLayout]
-         materialRippleLayout.findViewById(R.id.image).asInstanceOf[ImageView].setImageResource(ICONS(position))
-         materialRippleLayout
+      if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+        val customTabLayout: FrameLayout = LayoutInflater.from(getActivity).inflate(R.layout.custom_tab_old, parent, false).asInstanceOf[FrameLayout]
+        customTabLayout.findViewById(R.id.image).asInstanceOf[ImageView].setImageResource(ICONS(position))
+        return customTabLayout
+      } else {
+        val materialRippleLayout: MaterialRippleLayout = LayoutInflater.from(getActivity).inflate(R.layout.custom_tab, parent, false).asInstanceOf[MaterialRippleLayout]
+        materialRippleLayout.findViewById(R.id.image).asInstanceOf[ImageView].setImageResource(ICONS(position))
+        return materialRippleLayout
+      }
+
+      null
     }
 
     override def getPageTitle(position: Int): CharSequence = {
@@ -44,22 +51,10 @@ class LeftPaneFragment extends Fragment {
 
   override def onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle): View = {
     val thisActivity = this.getActivity.asInstanceOf[MainActivity]
-    val actionBar = thisActivity.getActionBar
+    val actionBar = thisActivity.getSupportActionBar
     val rootView = inflater.inflate(R.layout.fragment_leftpane, container, false)
     val pager = rootView.findViewById(R.id.pager).asInstanceOf[ViewPager]
     val tabs = rootView.findViewById(R.id.pager_tabs).asInstanceOf[PagerSlidingTabStrip]
-
-    val tabListener = new ActionBar.TabListener() {
-        def onTabSelected(tab: ActionBar.Tab, ft: FragmentTransaction) = {
-          pager.setCurrentItem(tab.getPosition)
-        }
-
-        def onTabUnselected(tab: ActionBar.Tab, ft: FragmentTransaction) = {
-        }
-
-        def onTabReselected(tab: ActionBar.Tab, ft: FragmentTransaction) = {
-        }
-    }
 
     pager.setAdapter(new LeftPagerAdapter(getFragmentManager))
     tabs.setViewPager(pager)
