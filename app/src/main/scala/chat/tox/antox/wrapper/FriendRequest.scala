000f451975940ca8018226aeafe1ebb8d1d65132
wiiam <wiiam@tuta.io>
2015-10-11 17:10:48 +1300

    Change key to FriendKey

--- app/src/main/scala/chat/tox/antox/wrapper/FriendRequest.scala
+++ app/src/main/scala/chat/tox/antox/wrapper/FriendRequest.scala
@@ -2,4 +2,4 @@ package chat.tox.antox.wrapper
 
 import chat.tox.antox.utils.ToxPublicKey
 
-class FriendRequest(val requestKey: ToxPublicKey, val requestMessage: String)
+class FriendRequest(val requestKey: FriendKey, val requestMessage: String)
