44efb03251c7d8b3f4d081f6dde9e578eb37629e
subliun <subliundev@gmail.com>
2015-10-16 21:48:20 +1030

    fix the build

--- app/src/main/scala/chat/tox/antox/utils/BriteScalaDatabase.scala
+++ app/src/main/scala/chat/tox/antox/utils/BriteScalaDatabase.scala
@@ -1,12 +1,11 @@
 package chat.tox.antox.utils
 
 import java.lang.Iterable
-import java.util.concurrent.TimeUnit
 
 import android.content.ContentValues
 import android.database.Cursor
 import com.squareup.sqlbrite.BriteDatabase
-import com.squareup.sqlbrite.SqlBrite.Query
+import rx.functions.Func1
 import rx.lang.scala.JavaConversions._
 import rx.lang.scala.Observable
 
@@ -17,11 +16,15 @@ class BriteScalaDatabase(db: BriteDatabase) {
 
   def close(): Unit = db.close()
 
-  def createQuery(table: String, sql: String, args: String*): Observable[Query] =
-    db.createQuery(table, sql, args: _*)
+  def createQuery(table: String, sql: String, args: String*): Observable[Cursor] =
+    db.createQuery(table, sql, args: _*).mapToOne(new Func1[Cursor, Cursor] {
+      override def call(cursor: Cursor): Cursor = cursor
+    })
 
-  def createQuery(tables: Iterable[String], sql: String, args: String*): Observable[Query] =
-    db.createQuery(tables, sql, args: _*)
+  def createQuery(tables: Iterable[String], sql: String, args: String*): Observable[Cursor] =
+    db.createQuery(tables, sql, args: _*).mapToOne(new Func1[Cursor, Cursor] {
+      override def call(cursor: Cursor): Cursor = cursor
+    })
 
   def delete(table: String, whereClause: String, whereArgs: String*): Int =
     db.delete(table, whereClause, whereArgs: _*)
