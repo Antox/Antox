d2f1abb0d7c82262d207b40b7fcaf6f5437d5b7a
subliun <subliundev@gmail.com>
2015-11-27 08:57:01 +1030

    Update dependency scripts for jenkins build name changes.

--- download-dependencies.sh
+++ download-dependencies.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 mkdir -p app/src/main/jniLibs/armeabi
 rm app/src/main/jniLibs/armeabi/libtox4j.so
-wget https://build.tox.chat/job/tox4j_build_android_arm_release/lastSuccessfulBuild/artifact/artifacts/armeabi/libtox4j.so -O app/src/main/jniLibs/armeabi/libtox4j.so
+wget https://build.tox.chat/job/tox4j_build_android_armel_release/lastSuccessfulBuild/artifact/artifacts/libtox4j.so -O app/src/main/jniLibs/armeabi/libtox4j.so
 cd ${0%/*}
 mkdir -p app/libs
 rm app/libs/tox4j_2.11.jar
-wget https://build.tox.chat/job/tox4j_build_android_arm_release/lastSuccessfulBuild/artifact/artifacts/tox4j_2.11-0.1-SNAPSHOT.jar -O app/libs/tox4j_2.11.jar
+wget https://build.tox.chat/job/tox4j_build_android_armel_release/lastSuccessfulBuild/artifact/artifacts/tox4j_2.11-0.1-SNAPSHOT.jar -O app/libs/tox4j_2.11.jar
 rm app/libs/protobuf-java-2.6.1.jar
-wget https://build.tox.chat/job/tox4j_build_android_arm_release/lastSuccessfulBuild/artifact/artifacts/protobuf.jar -O app/libs/protobuf-java-2.6.1.jar
+wget https://build.tox.chat/job/protobuf_build_android_armel_release/lastSuccessfulBuild/artifact/protobuf.jar -O app/libs/protobuf-java-2.6.1.jar
+
