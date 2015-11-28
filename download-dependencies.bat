44119e08c3ecfeb7a7e68fa887b107b7fbab06fa
subliun <subliundev@gmail.com>
2015-11-29 01:29:11 +1030

    Remove libs and update download-dependencies scripts for x86 and arm64

--- download-dependencies.bat
+++ download-dependencies.bat
@@ -1,11 +1,26 @@
 @echo off
 echo "Started..."
+
 mkdir app\src\main\jniLibs\armeabi
-DEL /F /Q app\src\main\jniLibs\armeabi\libtox4j.so
-echo "Removed old version: libtox4j.so"
-echo "Downloading latest version: libtox4j.so"
+mkdir app\src\main\jniLibs\x86
+mkdir app\src\main\jniLibs\arm64-v8a
+
+DEL /F /Q app/src/main/jniLibs/armeabi/libtox4j.so
+DEL /F /Q app/src/main/jniLibs/x86/libtox4j.so
+DEL /F /Q app/src/main/jniLibs/arm64-v8a/libtox4j.so
+
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://build.tox.chat/job/tox4j_build_android_armel_release/lastSuccessfulBuild/artifact/artifacts/libtox4j.so', 'app\src\main\jniLibs\armeabi\libtox4j.so')"
-echo "Downloaded."
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://build.tox.chat/job/tox4j_build_android_x86_release/lastSuccessfulBuild/artifact/artifacts/libtox4j.so', 'app\src\main\jniLibs\x86\libtox4j.so')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://build.tox.chat/job/tox4j_build_android_arm64_release/lastSuccessfulBuild/artifact/artifacts/libtox4j.so', 'app\src\main\jniLibs\arm64-v8a\libtox4j.so')"
+
+DEL /F /Q app/src/main/jniLibs/armeabi/libkaliumjni.so
+DEL /F /Q app/src/main/jniLibs/x86/libkaliumjni.so
+DEL /F /Q app/src/main/jniLibs/arm64-v8a/libkaliumjni.so
+
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://build.tox.chat/job/libkaliumjni_build_android_armel_static_release/lastSuccessfulBuild/artifact/kalium-jni/jni/libkaliumjni.so', 'app\src\main\jniLibs\armeabi\libkaliumjni.so')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://build.tox.chat/job/libkaliumjni_build_android_x86_static_release/lastSuccessfulBuild/artifact/kalium-jni/jni/libkaliumjni.so', 'app\src\main\jniLibs\x86\libkaliumjni.so')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://build.tox.chat/job/libkaliumjni_build_android_arm64_static_release/lastSuccessfulBuild/artifact/kalium-jni/jni/libkaliumjni.so', 'app\src\main\jniLibs\arm64-v8a\libkaliumjni.so')"
+
 mkdir app\libs
 DEL /F /Q app\libs\tox4j_2.11.jar
 echo "Removed old version: tox4j_2.11.jar"
@@ -16,6 +31,6 @@ DEL /F /Q app\libs\protobuf-java-2.6.1.jar
 echo "Removed old version: protobuf-java-2.6.1.jar"
 echo "Downloading latest version: protobuf-java-2.6.1.jar"
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://build.tox.chat/job/protobuf_build_android_armel_release/lastSuccessfulBuild/artifact/protobuf.jar', 'app\libs\protobuf-java-2.6.1.jar')"
-echo "Downloaded."
+
 echo "...Finished!"
 @echo ON
--- download-dependencies.sh
+++ download-dependencies.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 mkdir -p app/src/main/jniLibs/armeabi
+mkdir -p app/src/main/jniLibs/x86
+mkdir -p app/src/main/jniLibs/arm64-v8a
+
 rm app/src/main/jniLibs/armeabi/libtox4j.so
+rm app/src/main/jniLibs/x86/libtox4j.so
+rm app/src/main/jniLibs/arm64-v8a/libtox4j.so
+
 wget https://build.tox.chat/job/tox4j_build_android_armel_release/lastSuccessfulBuild/artifact/artifacts/libtox4j.so -O app/src/main/jniLibs/armeabi/libtox4j.so
+wget https://build.tox.chat/job/tox4j_build_android_x86_release/lastSuccessfulBuild/artifact/artifacts/libtox4j.so -O app/src/main/jniLibs/x86/libtox4j.so
+wget https://build.tox.chat/job/tox4j_build_android_arm64_release/lastSuccessfulBuild/artifact/artifacts/libtox4j.so -O app/src/main/jniLibs/arm64-v8a/libtox4j.so
+
+rm app/src/main/jniLibs/armeabi/libkaliumjni.so
+rm app/src/main/jniLibs/x86/libkaliumjni.so
+rm app/src/main/jniLibs/arm64-v8a/libkaliumjni.so
+
+wget https://build.tox.chat/job/libkaliumjni_build_android_armel_static_release/lastSuccessfulBuild/artifact/kalium-jni/jni/libkaliumjni.so -O app/src/main/jniLibs/armeabi/libkaliumjni.so
+wget https://build.tox.chat/job/libkaliumjni_build_android_x86_static_release/lastSuccessfulBuild/artifact/kalium-jni/jni/libkaliumjni.so -O app/src/main/jniLibs/x86/libkaliumjni.so
+wget https://build.tox.chat/job/libkaliumjni_build_android_arm64_static_release/lastSuccessfulBuild/artifact/kalium-jni/jni/libkaliumjni.so -O app/src/main/jniLibs/arm64-v8a/libkaliumjni.so
+
 cd ${0%/*}
 mkdir -p app/libs
 rm app/libs/tox4j_2.11.jar
