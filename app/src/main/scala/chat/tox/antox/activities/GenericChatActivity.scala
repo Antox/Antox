ca4938199014b0c3f722548188b13332f0b8ed3c
subliun <subliundev@gmail.com>
2015-12-14 23:55:07 +1030

    Move export profile strings into strings.xml for i18n.

--- app/src/main/scala/chat/tox/antox/activities/GenericChatActivity.scala
+++ app/src/main/scala/chat/tox/antox/activities/GenericChatActivity.scala
@@ -63,6 +63,7 @@ abstract class GenericChatActivity[KeyType <: ContactKey] extends AppCompatActiv
     activeKey = getKey(extras.getString("key"))
     fromNotifications = extras.getBoolean("notification", false)
     val thisActivity = this
+
     AntoxLog.debug("key = " + activeKey)
 
     val db = State.db
