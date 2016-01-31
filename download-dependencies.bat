a87627af0e8e4da2d1c5d3af131aea82d3b3a9ea
wiiaam <wiiam24@gmail.com>
2016-01-31 19:04:48 +1300

    Remove quotation marks from windows dependencies script

--- download-dependencies.bat
+++ download-dependencies.bat
@@ -1,5 +1,5 @@
 @echo off
-echo "Started..."
+echo Started...
 
 mkdir app\src\main\jniLibs\armeabi
 mkdir app\src\main\jniLibs\x86
@@ -9,7 +9,7 @@ DEL /F /Q app/src/main/jniLibs/armeabi/libtox4j.so
 DEL /F /Q app/src/main/jniLibs/x86/libtox4j.so
 DEL /F /Q app/src/main/jniLibs/arm64-v8a/libtox4j.so
 
-echo "Downloading latest version: libtox4j.so"
+echo Downloading latest version: libtox4j.so
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://build.tox.chat/job/tox4j_build_android_armel_release/lastSuccessfulBuild/artifact/artifacts/libtox4j.so', 'app\src\main\jniLibs\armeabi\libtox4j.so')"
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://build.tox.chat/job/tox4j_build_android_x86_release/lastSuccessfulBuild/artifact/artifacts/libtox4j.so', 'app\src\main\jniLibs\x86\libtox4j.so')"
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://build.tox.chat/job/tox4j_build_android_arm64_release/lastSuccessfulBuild/artifact/artifacts/libtox4j.so', 'app\src\main\jniLibs\arm64-v8a\libtox4j.so')"
@@ -18,17 +18,17 @@ DEL /F /Q app/src/main/jniLibs/armeabi/libkaliumjni.so
 DEL /F /Q app/src/main/jniLibs/x86/libkaliumjni.so
 DEL /F /Q app/src/main/jniLibs/arm64-v8a/libkaliumjni.so
 
-echo "Downloading latest version: libkaliumjni.so"
+echo Downloading latest version: libkaliumjni.so
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://build.tox.chat/job/libkaliumjni_build_android_armel_static_release/lastSuccessfulBuild/artifact/kalium-jni/jni/libkaliumjni.so', 'app\src\main\jniLibs\armeabi\libkaliumjni.so')"
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://build.tox.chat/job/libkaliumjni_build_android_x86_static_release/lastSuccessfulBuild/artifact/kalium-jni/jni/libkaliumjni.so', 'app\src\main\jniLibs\x86\libkaliumjni.so')"
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://build.tox.chat/job/libkaliumjni_build_android_arm64_static_release/lastSuccessfulBuild/artifact/kalium-jni/jni/libkaliumjni.so', 'app\src\main\jniLibs\arm64-v8a\libkaliumjni.so')"
 
 mkdir app\libs
 DEL /F /Q app\libs\tox4j_2.11.jar
-echo "Removed old version: tox4j_2.11.jar"
-echo "Downloading latest version: tox4j_2.11.jar"
+echo Removed old version: tox4j_2.11.jar
+echo Downloading latest version: tox4j_2.11.jar
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://build.tox.chat/job/tox4j_build_android_armel_release/lastSuccessfulBuild/artifact/artifacts/tox4j_2.11-0.1-SNAPSHOT.jar', 'app\libs\tox4j_2.11.jar')"
-echo "Downloaded."
-echo "...Finished!"
+echo Downloaded.
+echo ...Finished!
 pause
 @echo ON
