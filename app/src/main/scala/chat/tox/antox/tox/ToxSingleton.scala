1c2b799b002c572e745b664e4bf05a8e57313c80
Zoff <zoff99@users.noreply.github.com>
2016-04-27 19:23:54 +0200

    enable 3g connection as default when DEBUG is on

--- app/src/main/scala/chat/tox/antox/tox/ToxSingleton.scala
+++ app/src/main/scala/chat/tox/antox/tox/ToxSingleton.scala
@@ -133,7 +133,14 @@ object ToxSingleton {
 
   def isToxConnected(preferences: SharedPreferences, context: Context): Boolean = {
     val connManager = context.getSystemService(Context.CONNECTIVITY_SERVICE).asInstanceOf[ConnectivityManager]
-    val wifiOnly = preferences.getBoolean("wifi_only", true)
+    val wifiOnly = _
+    if (MainActivity.DEBUG_CI == 1) {
+      // automate
+      wifiOnly = preferences.getBoolean("wifi_only", false)
+      // automate
+    } else {
+      wifiOnly = preferences.getBoolean("wifi_only", true)
+    }
     val wifiInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
 
     !(wifiOnly && !wifiInfo.isConnected)
