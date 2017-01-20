a9977a10acca407aeb5eb0bc56b6616d464e138e
Zoff <zoff99@users.noreply.github.com>
2017-01-20 12:41:24 +0100

    fix startup issues on API4.x devices

--- app/src/main/scala/chat/tox/antox/activities/SplashActivity.java
+++ app/src/main/scala/chat/tox/antox/activities/SplashActivity.java
@@ -3,6 +3,7 @@ package chat.tox.antox.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 
 /**
  * Created by zoff99 on 08.01.2017.
@@ -10,11 +11,17 @@ import android.support.v7.app.AppCompatActivity;
 public class SplashActivity extends AppCompatActivity
 {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
         Intent intent = new Intent(this, chat.tox.antox.activities.LoginActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    static
+    {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
     }
 }
