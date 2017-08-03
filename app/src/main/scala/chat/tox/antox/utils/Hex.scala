61aaea07039456147957fdf0fd37033f6ca75ac8
wiiaam <wiiam24@gmail.com>
2017-08-04 00:31:07 +1200

    Fix hex conversion error

--- app/src/main/scala/chat/tox/antox/utils/Hex.scala
+++ app/src/main/scala/chat/tox/antox/utils/Hex.scala
@@ -2,8 +2,8 @@ package chat.tox.antox.utils
 
 object Hex {
   def hexStringToBytes(s: String): Array[Byte] =
-    s.grouped(4).map(Integer.parseInt(_, 16).toByte).toArray
+    s.grouped(2).map(Integer.parseInt(_, 16).toByte).toArray
 
   def bytesToHexString(bytes: Array[Byte]): String =
-    bytes.map("%02X".format(_)).mkString
+    bytes.map("%02x".format(_)).mkString
 }
