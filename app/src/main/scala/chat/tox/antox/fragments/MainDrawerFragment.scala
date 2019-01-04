fc18ce45f142a3dfd9ebbcbac2d648fa99a0b645
askymore <askymore@sina.com>
2019-01-04 16:54:54 +0800

    bugfix--the maindrawer does not refresh the username and status when the drawer open

--- app/src/main/scala/chat/tox/antox/fragments/MainDrawerFragment.scala
+++ app/src/main/scala/chat/tox/antox/fragments/MainDrawerFragment.scala
@@ -138,6 +138,14 @@ class MainDrawerFragment extends Fragment {
 
   def openDrawer(): Unit = {
     mDrawerLayout.openDrawer(GravityCompat.START)
+    // bugfix--the maindrawer does not refresh the username and status when the drawer open
+    userDetailsSubscription = State.userDb(getActivity)
+      .activeUserDetailsObservable()
+      .combineLatestWith(AntoxOnSelfConnectionStatusCallback.connectionStatusSubject)((user, status) => (user, status))
+      .observeOn(AndroidMainThreadScheduler())
+      .subscribe((tuple) => {
+        refreshDrawerHeader(tuple._1, tuple._2)
+      })
   }
 
   def closeDrawer(): Unit = {
